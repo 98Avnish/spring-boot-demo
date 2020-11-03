ffad7d39e2690510cd74080cb94628c35d898b97
JAvnish <avnishjain1998@gmail.com>
2020-11-03 16:06:01 +0530

    Added Cucumber Test for Calculator class

---
+++ src/test/java/com/example/demo/cucumber/CalculatorStepDefinitions.java
@@ -0,0 +1,28 @@
+package com.example.demo.cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorStepDefinitions {
+
+    private Calculator calculator;
+    private Integer result;
+
+    @Given("I have a calculator")
+    public void i_have_a_calculator() {
+        this.calculator = new Calculator();
+    }
+
+    @When("I add {int} and {int}")
+    public void i_add_and(Integer int1, Integer int2) {
+        this.result = this.calculator.add(int1, int2);
+    }
+
+    @Then("I should get {int}")
+    public void i_should_get(Integer expected) {
+        assertThat(this.result).isEqualTo(expected);
+    }
+}
